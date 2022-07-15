8b0cd3e857c497e1e970da539274ba7dcae7b235
ZFG123456 <798573918@qq.com>
2022-07-15 16:12:13 +0800

    1.1版本

--- src/com/zfg/testStudy/Car.java
+++ src/com/zfg/testStudy/Car.java
@@ -8,6 +8,14 @@ package com.zfg.testStudy;
  */
 public interface Car {
 
+    /***
+    * @Description: //汽车行驶功能
+    * @MethodName: drive
+    * @Param: []
+    * @return: void
+    * @Author: zfg
+    * @Date: 2022/7/15 下午 04:09
+    */
     public void drive();
 
 }
