137ceae31b4c90aa2e00a22184ec3163795da2af
ZFG123456 <798573918@qq.com>
2022-07-27 15:07:47 +0800

    1.1版本测试链表操作

--- src/com/zfg/testStudy/TestTicket.java
+++ src/com/zfg/testStudy/TestTicket.java
@@ -1,28 +1,34 @@
 package com.zfg.testStudy;
 
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @ClassName TestTicket
  * @Description TODO
  * @Author zfg
  * @Date 2022/7/16 下午 03:46
  */
-public class TestTicket implements Runnable{
+public class TestTicket implements Runnable {
 
-
-    private int ticketNum = 100;
+    //使用lock锁更加灵活
+//    Lock l = new ReentrantLock();
+    private volatile int ticketNum = 100;
 
     @Override
     public synchronized void run() {
-
+//        l.lock();
         long t1 = System.currentTimeMillis();
         //卖票逻辑
-        while (ticketNum>0){
-            System.out.println(Thread.currentThread().getName()+"拿到了第"+ticketNum--+"张票！");
+        while (ticketNum > 0) {
+            System.out.println(Thread.currentThread().getName() + "拿到了第" + ticketNum-- + "张票！");
         }
         long t2 = System.currentTimeMillis();
 
         long time = t2-t1;
         System.out.println("本次抢票总共用时:"+time);
+//        l.unlock();
 
     }
 
@@ -33,10 +39,12 @@ public class TestTicket implements Runnable{
 //        TestTicket t3 = new TestTicket();
 
 
-        new Thread(t1,"张三").start();
-        new Thread(t1,"李四").start();
-        new Thread(t1,"王五").start();
+        new Thread(t1, "张三").start();
+        new Thread(t1, "李四").start();
+        new Thread(t1, "王五").start();
 
     }
 
 }
+
+//alt+shift+insert竖向修改
